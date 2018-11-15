b8f451a563ea603b06d3e3a7cf9e18b9f94a6faa
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-11-15 23:19:10 +0100

    hostname

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
+const ME = process.env.HOSTNAME;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+  res.send('Hello world - '+ME+'\n');
 });
 
 app.listen(PORT, HOST);
