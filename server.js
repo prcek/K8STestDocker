f7619f360885777c6f8b1e46a55c1d94c91260c1
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-11-16 14:23:53 +0100

    env test

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const ME = process.env.HOSTNAME;
+const HOSTNAME = process.env.HOSTNAME;
+const APP_NAME = process.evn.APP_NAME;
 // App
 const app = express();
 app.get('/', (req, res) => {
 
-  res.send('Hello world - '+ME+'\n');
+  res.send(''+APP_NAME+'Hello world - '+HOSTNAME+'\n');
 });
 
 app.listen(PORT, HOST);
